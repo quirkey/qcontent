46f08913f0e9e5d3729a57d576d115724977ecfe
Aaron Quint <aaron@quirkey.com>
2009-05-29 15:39:33 -0400

    Added an :allow_symbol option to persitance

--- lib/qcontent/persistance.rb
+++ lib/qcontent/persistance.rb
@@ -9,27 +9,51 @@ module Qcontent
     module Macros
       def persist(name, options = {})
         name        = name.to_s
-        session_var = options[:in]   || name.foreign_key
-        klass       = options[:type] || name.classify
+        session_var   = options[:in]   || name.foreign_key
+        klass         = options[:type] || name.classify
+        allow_symbols = options[:allow_symbols] || false
 
-        module_eval <<-EOT
+        to_eval = <<-EOT
         helper_method :#{name}
 
         protected
         def #{name}
-          @_#{name} ||= (session[:#{session_var}] ? #{klass}.find(session[:#{session_var}]) : nil)
+        EOT
+        if allow_symbols
+          to_eval << "@_#{name} ||= (session[:#{session_var}] ? (session[:#{session_var}].is_a?(Symbol) ? session[:#{session_var}] : #{klass}.find(session[:#{session_var}])) : nil)"
+        else
+          to_eval << "@_#{name} ||= (session[:#{session_var}] ? #{klass}.find(session[:#{session_var}]) : nil)"
         end
-
+        to_eval << <<-EOT
+        end
+      
         def #{name}=(set_object)
           @_#{name} = nil
-          session[:#{session_var}] = (set_object.is_a?(#{klass}) ? set_object.id : nil)
+        EOT
+        
+        if allow_symbols
+          to_eval << <<-EOT
+          session[:#{session_var}] = if set_object.is_a?(#{klass})
+            set_object.id
+          elsif set_object.is_a?(Symbol)
+            set_object
+          else
+            nil
+          end
+          EOT
+        else
+          to_eval << "session[:#{session_var}] = (set_object.is_a?(#{klass}) ? set_object.id : nil)"
         end
-
+        
+        to_eval << <<-EOT
+        end
+        
         def clear_#{name}!
           @_#{name} = nil
           session.delete(:#{session_var})
         end
         EOT
+        module_eval to_eval
       end
     end
 
